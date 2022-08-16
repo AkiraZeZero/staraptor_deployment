692d916786682344482226236df39bf381ad46da
AkiraZeZero <kiara.ocasio07@gmail.com>
2022-08-16 16:30:48 -0400

    final commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('client'))
 app.use('/js', express.static(path.join(__dirname, 'client/main.js')))
-
+app.use(`/css`, express.static(path.join(__dirname, 'client/style.css')))
 
 app.get(`/`, (req, res) => {
     res.sendFile(path.join(__dirname, `../client/index.html`))
@@ -18,6 +18,9 @@ app.get(`/js`, (req, res) => {
     res.sendFile(path.join(__dirname, `../client/main.js`))
 })
 
+app.get(`/css`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../client/style.css`))
+})
 
 const port = process.env.PORT || 4005
 
